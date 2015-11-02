5375d45441dc1fd3b04b509b77b2889edd27dd4e
AugustT <tomaug@ceh.ac.uk>
2015-11-02 14:06:20 +0000

    Add skips to all tests that require login details. This will not be run on Travis

--- tests/testthat/testgetFeature.R
+++ tests/testthat/testgetFeature.R
@@ -1,9 +1,9 @@
 context("Test getFeature")
 
-# login
-load('~/rnbn_test.rdata')
-nbnLogin(username = UN_PWD$username, password = UN_PWD$password)
-
 test_that("Get details for a feature", {
+    if(!file.exists('~/rnbn_test.rdata')) skip('login details not found')
+    # login
+    load('~/rnbn_test.rdata')
+    nbnLogin(username = UN_PWD$username, password = UN_PWD$password)
     expect_equal(as.character(getFeature("97479")['label']), "SN413499")    
 })
--- tests/testthat/testrunnbnurl.R
+++ tests/testthat/testrunnbnurl.R
@@ -1,5 +1,11 @@
 context("Test runnbnurl")
+
 test_that("runnbnurl gets a response", {
+    if(!file.exists('~/rnbn_test.rdata')) skip('login details not found')
+    # login
+    load('~/rnbn_test.rdata')
+    nbnLogin(username = UN_PWD$username, password = UN_PWD$password)
+    
     expect_that(length(runnbnurl(service="obs", tvks="NBNSYS0000002987", datasets="GA000373", startYear="1990", endYear="2010")) > 0, is_true()) 
 })
 
