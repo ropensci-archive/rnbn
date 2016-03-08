63c408e760dd4e7e470da9ff7e16924d4b6cdc99
Mathew Hall <mathew.james.hall@gmail.com>
2016-03-08 15:45:18 +0000

    Fix error message for getOccurrences with data frame

--- R/getOccurrences.R
+++ R/getOccurrences.R
@@ -63,7 +63,7 @@ getOccurrences <- function(tvks=NULL, datasets=NULL, startYear=NULL,
     if(!is.null(tvks) & !is.null(group)) stop('group and tvks cannot be used at the same time')
     if(is.null(tvks) & is.null(group) & is.null(gridRef) & is.null(polygon) & is.null(point)) stop('One of group, tvks or gridRef must be given')
     if(is.data.frame(tvks)){
-      if(!("ptaxonVersionKey" %in% names(tvks))) stop("pTaxonVersionKey column missing from data.frame tvks")
+      if(!("ptaxonVersionKey" %in% names(tvks))) stop("ptaxonVersionKey column missing from data.frame tvks")
     
       tvks <- unique(as.character(tvks$ptaxonVersionKey))
     } 
