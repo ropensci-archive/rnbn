14f98f3113d4f17deeb4df6a7278d9ee06bffafc
AugustT <tomaug@ceh.ac.uk>
2017-01-05 12:26:40 +0000

    update onload message

--- R/onLoad_information.R
+++ R/onLoad_information.R
@@ -1,4 +1,4 @@
 # things to do on loading the package
 .onLoad <- function(libname, pkgname) {
-    packageStartupMessage("The NBN is moving to new APIs in 2017, as a result this package will be depricated and a new package created for the new APIs. For more information see https://github.com/ropensci/rnbn/issues/37")
+    packageStartupMessage("The NBN is moving to new APIs in 2017, as a result this package will stop working at the end of March 2017. A new package will be created for the new APIs. For more information see https://github.com/ropensci/rnbn/issues/37")
 }
